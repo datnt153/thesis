3b49fff12e725d8bf4a2e4a73280ca48f11cc1b9
datnt114 <datnt114@gmail.com>
2023-12-07 18:01:29 +0700

    add ensemble

--- final_inference.py
+++ final_inference.py
@@ -9,6 +9,8 @@ from data import MyDataset
 import numpy as np
 from tqdm import tqdm
 import argparse
+import torch.nn.functional as F
+
 
 def val_epoch(loader, model, device, use_pose, criterion):
     model.eval()
@@ -22,7 +24,11 @@ def val_epoch(loader, model, device, use_pose, criterion):
         for data in bar:
             if use_pose:
                 img, pose, target = data
-                img, pose, target = img.to(device), pose.to(device), target.long().to(device)
+                img, pose, target = (
+                    img.to(device),
+                    pose.to(device),
+                    target.long().to(device),
+                )
             else:
                 img, target = data
                 img, target = img.to(device), target.long().to(device)
@@ -51,7 +57,7 @@ def val_epoch(loader, model, device, use_pose, criterion):
     cm = confusion_matrix(TARGETS, PREDS.argmax(1))
     print(cm)
 
-    return val_loss, acc, PREDS.argmax(1) , TARGETS
+    return val_loss, acc, PREDS.argmax(1) , TARGETS, LOGITS
 
 def main(args):
     print(f"args: {args}")
@@ -67,48 +73,58 @@ def main(args):
     # Define loss criterion
     criterion = torch.nn.CrossEntropyLoss().to(device=device)
 
-    views = ['Dashboard', 'Rear_view', 'Right_side_window']
+    views = ["Dashboard", "Rear_view", "Right_side_window"]
 
     # Define all the scenarios you want to run
     img_size = 256
     configs = [
+        {
+            "model_name": "tf_efficientnetv2_l_in21k",
+            "bs": 16,
+            "path": [
+                {"time_line": "2023_09_14_09.48", "use_pose": True},
+                # {"time_line": "2023_09_14_17.18", "use_pose": False},
+            ],
+            "gpus": 1,
+        },
+        # {
+        #     "model_name": "tf_efficientnetv2_m_in21k",
+        #     "bs": 48,
+        #     "path": [{"time_line": "2023_09_09_16.11", "use_pose": True}, {"time_line": "2023_09_09_19.32", "use_pose": False} ],
+        #     "gpus": 2
+        # },
             # {
-            #     "model_name": "tf_efficientnetv2_l_in21k",
-            #     "bs": 16,
-            #     "path": [{"time_line": "2023_09_14_09.48", "use_pose": True}, {"time_line": "2023_09_14_17.18", "use_pose": False} ],
-            #     "gpus": 1
-
-            # },
-            {
-
-                "model_name": "tf_efficientnetv2_m_in21k",
-                "bs": 48,
-                "path": [{"time_line": "2023_09_09_16.11", "use_pose": True}, {"time_line": "2023_09_09_19.32", "use_pose": False} ],
-                "gpus": 2
-            },
             # {
 
-            #     "model_name": "tf_efficientnetv2_s_in21k",
-            #     "bs": 48,
-            #     "path": [{"time_line": "2023_09_13_17.46", "use_pose": True}, {"time_line": "2023_09_13_20.17", "use_pose": False} ],
-            #     "gpus": 2
-            # }
-            ]
+        # {
+
+        #     "model_name": "tf_efficientnetv2_s_in21k",
+        #     "bs": 48,
+        #     "path": [{"time_line": "2023_09_13_17.46", "use_pose": True}, {"time_line": "2023_09_13_20.17", "use_pose": False} ],
+        #     "gpus": 2
+        # }
+    ]
 
     for config in configs:
-        modelname = config['model_name']
-        batch_size = config['bs']
-        for path in config['path']:
-            timeline = path['time_line']
-            use_pose = path['use_pose']
+        modelname = config["model_name"]
+        batch_size = config["bs"]
+
+        for path in config["path"]:
+            timeline = path["time_line"]
+            use_pose = path["use_pose"]
+
             folder_name = f"image-{modelname}"
+
             if use_pose:
                 folder_name = f"pose-{modelname}"
+
+            all_logits = {folder_name: []}
+            all_targets = None  # Assuming targets are same for all views
+
             for view in views:
-                
                 # Modify the model_path based on your directory structure
-                dir_model_path = f'final_models/{modelname} imgs {img_size} bs {batch_size}/{timeline}/{folder_name}/{view}'
-                model_path = f'{dir_model_path}/best_{modelname}.pth'
+                dir_model_path = f"final_models/{modelname} imgs {img_size} bs {batch_size}/{timeline}/{folder_name}/{view}"
+                model_path = f"{dir_model_path}/best_{modelname}.pth"
 
                 # Load the saved model
                 model = Model(model_name=modelname, use_pose=use_pose)
@@ -127,34 +143,71 @@ def main(args):
                 len_val = len(df_val)
                 print(f"Val: {len_val}")
 
-                dataset_valid = MyDataset(df=df_val, view=view, img_size=img_size, mode="valid", use_pose=use_pose, data_path=data_path)
-                valid_loader = DataLoader(dataset_valid, batch_size=batch_size,
-                                          sampler=SequentialSampler(dataset_valid), num_workers=num_workers)
+                dataset_valid = MyDataset(
+                    df=df_val,
+                    view=view,
+                    img_size=img_size,
+                    mode="valid",
+                    use_pose=use_pose,
+                    data_path=data_path,
+                )
+                valid_loader = DataLoader(
+                    dataset_valid,
+                    batch_size=batch_size,
+                    sampler=SequentialSampler(dataset_valid),
+                    num_workers=num_workers,
+                )
 
                 # Validate the model
-                val_loss, accuracy, PREDS, TARGETS = val_epoch(valid_loader, model, device, use_pose, criterion)
-                print(PREDS)
-                print(TARGETS)
-                print(type(PREDS), type(TARGETS))
-                data = {'predict': PREDS, "targets": TARGETS}  
-                new = pd.DataFrame(data)
-                new.to_csv(f"predicts/{folder_name}_view_{view}.csv")
+                val_loss, accuracy, PREDS, TARGETS, logits = val_epoch(
+                    valid_loader, model, device, use_pose, criterion
+                )
+                # print(PREDS)
+                # print(TARGETS)
+                # print(type(PREDS), type(TARGETS))
+                print(logits)
+                print(f"{folder_name}_view_{view}: {accuracy}")
+                # data = {'predict': PREDS, "targets": TARGETS}
+                # new = pd.DataFrame(data)
+                # new.to_csv(f"predicts/{folder_name}_view_{view}.csv")
 
+                all_logits[folder_name].append(logits)
+                all_targets = (
+                    TARGETS  # Update targets - assumed to be same for all views
+                )
+                
+                # print(logtis.shape)
 
-                print(f"model path: {dir_model_path}")
-                print(f"Validation loss: {val_loss}, Accuracy: {accuracy}")
-                print()
+            avg_logits = np.mean(all_logits[folder_name], axis=0)
+            print(avg_logits.shape)
+            avg_predict = avg_logits.argmax(1)
+            print(avg_predict)
+            acc = np.mean(avg_predict == all_targets) * 100  # Calculate accuracy
+            print(f"Accuracy for {folder_name}: {acc}%")
+
+            # Save to CSV
+            data = {"target": all_targets, "avg_predict": avg_predict}
+            df = pd.DataFrame(data)
+            df.to_csv(f"{folder_name}_embedded.csv", index=False)
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Inference model")
-    
+
     parser.add_argument("--img_size", type=int, default=512, help="Image size")
-    parser.add_argument("--data_path",type=str, default="/home/datnt114/thesis/aicity2023/code/tmp", help="Data path")
+    parser.add_argument(
+        "--data_path",
+        type=str,
+        default="/home/datnt114/thesis/aicity2023/code/tmp",
+        help="Data path",
+    )
     parser.add_argument("--use_pose", action="store_true", help="Use pose dataset")
     parser.add_argument("--batch_size", type=int, default=4, help="Batch size")
     parser.add_argument("--num_workers", type=int, default=40, help="Number of workers")
     parser.add_argument("--device", type=str, default="cuda", help="Device (cuda/cpu)")
-    parser.add_argument("--timeline", type=str, default="2023_09_11_15.50", help="Timeline train")
+    parser.add_argument(
+        "--timeline", type=str, default="2023_09_11_15.50", help="Timeline train"
+    )
 
     args = parser.parse_args()
     main(args)
